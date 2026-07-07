fc675bdc5798d2192f8ba47cebcd608422d3ecae
stevenmarp <100309799+stevenmarp@users.noreply.github.com>
2026-07-07 22:40:54 +0700

    Bump manifest version to 15.0.1.0.1

--- sm_legal_agreement/__manifest__.py
+++ sm_legal_agreement/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Dynamic Document Template Builder',
-    'version': '15.0.1.0.0',
+    'version': '15.0.1.0.1',
     'category': 'Productivity',
     'summary': 'Create dynamic document templates with placeholders mapped to any model fields',
     'description': """
