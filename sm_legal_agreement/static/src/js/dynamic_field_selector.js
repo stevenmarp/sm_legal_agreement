002285e113dac815389fc95452fdb297e81977eb
stevenmarp <100309799+stevenmarp@users.noreply.github.com>
2026-07-07 20:05:54 +0700

    Migrate to Odoo 15.0

--- sm_legal_agreement/static/src/js/dynamic_field_selector.js
+++ sm_legal_agreement/static/src/js/dynamic_field_selector.js
@@ -1,108 +1,115 @@
-/** @odoo-module **/
+odoo.define('sm_legal_agreement.DynamicFieldSelectorChar', function (require) {
+    "use strict";
 
-import { registry } from "@web/core/registry";
-import { CharField } from "@web/views/fields/char/char_field";
-import { useService } from "@web/core/utils/hooks";
-import { useState, useRef } from "@odoo/owl";
+    var AbstractField = require('web.AbstractField');
+    var field_registry = require('web.field_registry');
+    var core = require('web.core');
 
-/**
- * Dynamic Field Selector Widget
- * Provides autocomplete suggestions for model field paths
- */
-export class DynamicFieldSelectorChar extends CharField {
-    setup() {
-        super.setup();
-        this.orm = useService("orm");
-        this.state = useState({
-            suggestions: [],
-            showSuggestions: false,
-            loading: false,
-        });
-        this.inputRef = useRef("input");
-    }
-
-    get modelName() {
-        // Get model name from the record's model_name field
-        return this.props.record.data.model_name || "";
-    }
-
-    async onInput(ev) {
-        const value = ev.target.value;
-        await this.props.record.update({ [this.props.name]: value });
-        
-        if (value && this.modelName) {
-            await this.fetchFieldSuggestions(value);
-        } else {
-            this.state.suggestions = [];
-            this.state.showSuggestions = false;
-        }
-    }
-
-    async fetchFieldSuggestions(query) {
-        if (!this.modelName) return;
-        
-        this.state.loading = true;
-        try {
-            // Get fields from the model
-            const fields = await this.orm.call(
-                "ir.model.fields",
-                "search_read",
-                [[
-                    ["model", "=", this.modelName],
-                    ["name", "ilike", query.split(".").pop()],
-                    ["ttype", "not in", ["one2many", "many2many", "binary"]]
-                ]],
-                { fields: ["name", "field_description", "ttype", "relation"], limit: 10 }
-            );
-
-            this.state.suggestions = fields.map(f => ({
-                name: this.buildFieldPath(query, f.name),
-                label: f.field_description,
-                type: f.ttype,
-                relation: f.relation,
+    var DynamicFieldSelectorChar = AbstractField.extend({
+        template: 'DynamicFieldSelectorChar',
+        events: {
+            'input input': '_onInput',
+            'blur input': '_onBlur',
+            'focus input': '_onFocus',
+            'click .o_suggestion_item': '_onClickSuggestion',
+        },
+        init: function () {
+            this._super.apply(this, arguments);
+            this.suggestions = [];
+            this.showSuggestions = false;
+            this.loading = false;
+        },
+        _renderEdit: function () {
+            var self = this;
+            this.$el.html(core.qweb.render('DynamicFieldSelectorChar.edit', {
+                value: this.value || '',
+                placeholder: this.attrs.placeholder || 'e.g. partner_id.name',
+                suggestions: this.suggestions,
+                showSuggestions: this.showSuggestions,
+                loading: this.loading,
             }));
-            this.state.showSuggestions = this.state.suggestions.length > 0;
-        } catch (e) {
-            console.warn("Error fetching field suggestions:", e);
-            this.state.suggestions = [];
-        }
-        this.state.loading = false;
-    }
+            this.$input = this.$('input');
+        },
+        _renderReadonly: function () {
+            this.$el.text(this.value || '');
+        },
+        _getRecordData: function () {
+            var record = this.recordData;
+            return record.model_name || '';
+        },
+        _onInput: function (ev) {
+            var self = this;
+            var value = $(ev.currentTarget).val();
+            this._setValue(value);
+            
+            var modelName = this._getRecordData();
+            if (value && modelName) {
+                this.loading = true;
+                this._renderEdit();
+                
+                var queryField = value.split('.').pop();
+                this._rpc({
+                    model: 'ir.model.fields',
+                    method: 'search_read',
+                    args: [[
+                        ['model', '=', modelName],
+                        ['name', 'ilike', queryField],
+                        ['ttype', 'not in', ['one2many', 'many2many', 'binary']]
+                    ]],
+                    kwargs: {
+                        fields: ['name', 'field_description', 'ttype', 'relation'],
+                        limit: 10
+                    }
+                }).then(function (fields) {
+                    self.suggestions = _.map(fields, function (f) {
+                        return {
+                            name: self._buildFieldPath(value, f.name),
+                            label: f.field_description,
+                            type: f.ttype,
+                            relation: f.relation,
+                        };
+                    });
+                    self.showSuggestions = self.suggestions.length > 0;
+                    self.loading = false;
+                    self._renderEdit();
+                }, function () {
+                    self.loading = false;
+                    self._renderEdit();
+                });
+            } else {
+                this.suggestions = [];
+                this.showSuggestions = false;
+                this._renderEdit();
+            }
+        },
+        _buildFieldPath: function (currentPath, fieldName) {
+            var parts = currentPath.split('.');
+            parts.pop();
+            parts.push(fieldName);
+            return parts.join('.');
+        },
+        _onClickSuggestion: function (ev) {
+            var suggestionName = $(ev.currentTarget).data('name');
+            this._setValue(suggestionName);
+            this.showSuggestions = false;
+            this._renderEdit();
+        },
+        _onBlur: function () {
+            var self = this;
+            setTimeout(function () {
+                self.showSuggestions = false;
+                self._renderEdit();
+            }, 200);
+        },
+        _onFocus: function () {
+            if (this.suggestions.length > 0) {
+                this.showSuggestions = true;
+                this._renderEdit();
+            }
+        },
+    });
 
-    buildFieldPath(currentPath, fieldName) {
-        const parts = currentPath.split(".");
-        parts.pop();
-        parts.push(fieldName);
-        return parts.join(".");
-    }
+    field_registry.add('DynamicFieldSelectorChar', DynamicFieldSelectorChar);
 
-    async selectSuggestion(suggestion) {
-        await this.props.record.update({ [this.props.name]: suggestion.name });
-        this.state.showSuggestions = false;
-        
-        // If it's a relational field, allow further drilling
-        if (suggestion.relation) {
-            // Could show more suggestions here
-        }
-    }
-
-    onBlur() {
-        // Delay hiding to allow click on suggestion
-        setTimeout(() => {
-            this.state.showSuggestions = false;
-        }, 200);
-    }
-
-    onFocus() {
-        if (this.state.suggestions.length > 0) {
-            this.state.showSuggestions = true;
-        }
-    }
-}
-
-DynamicFieldSelectorChar.template = "sm_legal_agreement.DynamicFieldSelectorChar";
-DynamicFieldSelectorChar.props = {
-    ...CharField.props,
-};
-
-registry.category("fields").add("DynamicFieldSelectorChar", DynamicFieldSelectorChar);
+    return DynamicFieldSelectorChar;
+});
